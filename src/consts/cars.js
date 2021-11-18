c8ab36beefed951e82666cc42d8c22cd564c9c8d
1stghost <cliffnelson06@gmail.com>
2021-11-17 17:52:25 -1200

    here we go!

---
+++ src/consts/cars.js
@@ -0,0 +1,65 @@
+const cars = [
+    {
+      id: '1',
+      name: 'Lexus',
+      image: require('../assets/img1.jpg'),
+      price: '$50000',
+      liked: true,
+    },
+    {
+      id: '2',
+      name: 'Range rover',
+      image: require('../assets/img8.jpg'),
+      price: '$20000',
+      liked: false,
+    },
+    {
+      id: '3',
+      name: 'Toyota 4x4',
+      image: require('../assets/img11.png'),
+      price: '$20000',
+      liked: true,
+    },
+    {
+      id: '4',
+      name: 'Benz s250',
+      image: require('../assets/img12.jpg'),
+      price: '$15000',
+      liked: true,
+    },
+
+    {
+      id: '3',
+      name: 'Toyota Land cruiser',
+      image: require('../assets/img6.jpg'),
+      price: '$20000',
+      liked: true,
+    },
+    {
+      id: '5',
+      name: 'BMW',
+      image: require('../assets/img13.jpg'),
+      price: '$60000',
+      liked: true,
+    },
+
+
+    {
+      id: '6',
+      name: 'Mercedes Glc',
+      image: require('../assets/img4.jpg'),
+      price: '$10000',
+      liked: true,
+    },
+    {
+      id: '7',
+      name: 'Lexus',
+      image: require('../assets/img15.jpg'),
+      price: '$12000',
+      liked: true,
+    },
+   
+  ];
+  
+  export default cars;
+  
